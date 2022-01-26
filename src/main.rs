3edf83ecd900d96ab887c96a70042f53c7e81ae0
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-01-27 01:39:53 +0900

    日本時間表記削除

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use chrono::Local;
 
 fn main() {
-    let text = Local::now().format("%Y/%m/%d %H:%M:%S (日本時間)");
+    let text = Local::now().format("%Y/%m/%d %H:%M:%S");
     println!("{}", text);
 }
